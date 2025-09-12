dd26f6abb97af6bd4c926a03cb4b4c88c72ba4a0
Al-harsouhir <chatmimi329@gmail.com>
2025-09-12 15:15:49 +0200

    refactor: test communication site -> discord avec webhook
    marche pas encore

--- js/commande-serv-humain.js
+++ js/commande-serv-humain.js
@@ -1,30 +1,17 @@
-const WEBHOOK_URL = "https://discord.com/api/webhooks/1415363525123248168/DpojBUDlTJrCFu2vwvTLTcfdQR--gI8vRC4MGfDBqmCSWrRit3J4L9bGkOqnH4VxN9ch";
 
-function sendMessage() {
-  const pseudo = document.getElementById("pseudo").value;
-  const msg = document.getElementById("message").value;
+function Message() {
+  let pseudo = document.getElementById("pseudo").value;
+  let message = document.getElementById("message").value;
 
-  fetch(WEBHOOK_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      embeds: [{
-        title: "Nouveau message ✨",
-        fields: [
-          { name: "Pseudo", value: pseudo || "Anonyme" },
-          { name: "Message", value: msg || "(vide)" }
-        ],
-        color: 3447003
-      }]
-    })
-  })
-  .then(res => {
-    if (res.ok) {
-      alert("Message envoyé ✅");
-    } else {
-      alert("Erreur ❌");
-    }
-  });
-  debugger
+  const webhook = "https://canary.discord.com/api/webhooks/1415363525123248168/DpojBUDlTJrCFu2vwvTLTcfdQR--gI8vRC4MGfDBqmCSWrRit3J4L9bGkOqnH4VxN9ch";
+
+  const contents = `nouvelle commande :\nPseudo : ${pseudo}\nMessage : ${message}`;
+  const request = XMLHttpRequest();
+  request.open("POST", Webhook);
+  request.setRequestHeader('Constent-type', 'application/json');
+
+  const params = {
+    content: contents
+  }
+  request.send(JSON.stringify(params));
 }
-
