b98f8e315dc02fda800f5faae1da0437736313d9
Al-harsouhir <chatmimi329@gmail.com>
2025-09-12 15:52:58 +0200

    refactor: js pour la communication avec le site
    il y a un problème de cross "Blocage d’une requête multiorigine (Cross-Origin Request) : la politique « Same Origin » ne permet pas de consulter la ressource distante située sur https://canary.discord.com/api/webhooks/1415363525123248168/DpojBUDlTJrCFu2vwvTLTcfdQR--gI8vRC4MGfDBqmCSWrRit3J4L9bGkOqnH4VxN9ch. Raison : échec de la requête CORS. Code d’état : (null)."

--- js/commande-serv-humain.js
+++ js/commande-serv-humain.js
@@ -1,17 +1,17 @@
 
 function Message() {
-  let pseudo = document.getElementById("pseudo").value;
+  let name = document.getElementById("name").value;
   let message = document.getElementById("message").value;
 
   const webhook = "https://canary.discord.com/api/webhooks/1415363525123248168/DpojBUDlTJrCFu2vwvTLTcfdQR--gI8vRC4MGfDBqmCSWrRit3J4L9bGkOqnH4VxN9ch";
 
-  const contents = `nouvelle commande :\nPseudo : ${pseudo}\nMessage : ${message}`;
-  const request = XMLHttpRequest();
-  request.open("POST", Webhook);
+  const contents = `nouvelle commande :\nName : ${name}\nMessage : ${message}`;
+  const request = new XMLHttpRequest();
+  request.open("POST", webhook);
   request.setRequestHeader('Constent-type', 'application/json');
 
   const params = {
-    content: contents
+      content: contents
   }
   request.send(JSON.stringify(params));
 }
